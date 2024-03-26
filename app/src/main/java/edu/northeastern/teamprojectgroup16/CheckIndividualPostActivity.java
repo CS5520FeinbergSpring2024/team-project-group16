0d9150871de7e44005dd87d06bc8137ba6daed91
cecilia-uu <huikong1996@163.com>
2024-03-25 21:25:27 -0700

    completed the navigation bar bottom

--- app/src/main/java/edu/northeastern/teamprojectgroup16/CheckIndividualPostActivity.java
+++ app/src/main/java/edu/northeastern/teamprojectgroup16/CheckIndividualPostActivity.java
@@ -1,46 +1,26 @@
 package edu.northeastern.teamprojectgroup16;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-
 import edu.northeastern.teamprojectgroup16.databinding.ActivityCheckIndividualPostBinding;
-import edu.northeastern.teamprojectgroup16.databinding.ActivityMainBinding;
 
-
-public class MainActivity extends AppCompatActivity {
-    FirebaseAuth mAuth;
-    Button btnLogOut;
-    ActivityMainBinding binding;
-
+public class CheckIndividualPostActivity extends AppCompatActivity {
+    ActivityCheckIndividualPostBinding binding;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityCheckIndividualPostBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        btnLogOut = findViewById(R.id.buttonLogOut);
-        mAuth = FirebaseAuth.getInstance();
-        btnLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(MainActivity.this, Login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
         setListeners();
     }
+
     private void setListeners() {
         replaceFragment(new HomeFragment());
         // Listeners for navigation buttons
