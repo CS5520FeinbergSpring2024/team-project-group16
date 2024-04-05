7ed1e51a4c1348626ce15ac5e97b6a0de8d48954
倪鸣一 <592488450@qq.com>
2024-04-05 13:22:37 -0700

    Basic framework for posting

--- app/src/main/java/edu/northeastern/teamprojectgroup16/AddFragment.java
+++ app/src/main/java/edu/northeastern/teamprojectgroup16/AddFragment.java
@@ -2,10 +2,13 @@ package edu.northeastern.teamprojectgroup16;
 
 import static android.app.Activity.RESULT_OK;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +25,7 @@ import android.widget.ImageView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -31,15 +35,23 @@ import androidx.fragment.app.Fragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.UUID;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AddFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class AddFragment extends Fragment {
@@ -74,9 +86,51 @@ public class AddFragment extends Fragment {
 
         buttonPost.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (imageUri != null) {
-                    // Add your code to upload image to Firebase Storage
+            public void onClick(View v) {
+                if (imageUri != null && !editTextCaption.getText().toString().isEmpty()) {
+
+                    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+                    if(firebaseUser != null){
+                        String userId = firebaseUser.getUid();
+
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users").child(userId);
+                        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                UserModel currentUser = snapshot.getValue(UserModel.class);
+
+                                if(currentUser != null) {
+                                    List<String> serverNames = currentUser.getServerIds(); // Assuming serverIds actually contains the server names
+
+                                    CharSequence[] servers = serverNames.toArray(new CharSequence[0]);
+
+                                    AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+                                    dialog.setTitle("Choose a server");
+                                    dialog.setItems(servers, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            String selectedServer = servers[i].toString();
+                                            Toast.makeText(getActivity(), "You selected " + selectedServer, Toast.LENGTH_SHORT).show();
+
+                                            // Create a new Post with the server name, imageUri and following caption
+                                            //Post post = new Post(selectedServer, imageUri, editTextCaption.getText().toString());
+
+                                            // TODO: Implement the logic in posting to Firebase Storage or another backend service using the post object created
+                                        }
+                                    });
+                                    dialog.show();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                // Handle error
+                            }
+                        });
+                    }
+                } else {
+                    Toast.makeText(getActivity(), "Please select an image and caption before posting", Toast.LENGTH_SHORT).show();
                 }
             }
         });
