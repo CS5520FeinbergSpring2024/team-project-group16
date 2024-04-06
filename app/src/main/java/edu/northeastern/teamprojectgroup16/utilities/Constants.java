5016e9e6713c0948ee742fa11d6042d7ab55c99c
cecilia-uu <huikong1996@163.com>
2024-04-06 13:16:59 -0700

    Revert "Revert "make home page show properly""
    This reverts commit 9b784a0a69645dedea4124faf4c8d54a89a89bae.

---
+++ app/src/main/java/edu/northeastern/teamprojectgroup16/utilities/Constants.java
@@ -0,0 +1,6 @@
+package edu.northeastern.teamprojectgroup16.utilities;
+
+public class Constants {
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_COLLECTION_POST_IMAGES = "Post Images";
+}
