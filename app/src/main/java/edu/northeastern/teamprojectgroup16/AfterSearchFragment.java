1a26fbe1a9bce5f1e6c4dabb61d29f2e1b0240d6
kuiyuanyuedui <103470588+kuiyuanyuedui@users.noreply.github.com>
2024-04-05 22:42:55 -0700

    Create a new fragment class named AfterSearchFragment where it will display the search results

--- app/src/main/java/edu/northeastern/teamprojectgroup16/AfterSearchFragment.java
+++ app/src/main/java/edu/northeastern/teamprojectgroup16/AfterSearchFragment.java
@@ -1,9 +1,11 @@
 package edu.northeastern.teamprojectgroup16;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +13,37 @@ import androidx.fragment.app.Fragment;
 
 public class AfterSearchFragment extends Fragment {
 
-    @Nullable
+    private TextView textViewUsername;
+    private TextView textViewEmail;
+
+    public static AfterSearchFragment newInstance(String username, String email) {
+        AfterSearchFragment fragment = new AfterSearchFragment();
+        Bundle args = new Bundle();
+        args.putString("username", username);
+        args.putString("email", email);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @SuppressLint("MissingInflatedId")
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_after_search, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_after_search, container, false);
+
+        textViewUsername = view.findViewById(R.id.textViewUsername);
+        textViewEmail = view.findViewById(R.id.textViewEmail);
+
+        // Retrieve arguments (username, email) passed to this fragment
+        Bundle args = getArguments();
+        if (args != null) {
+            String username = args.getString("username");
+            String email = args.getString("email");
+
+            textViewUsername.setText(username);
+            textViewEmail.setText(email);
+        }
+
+        return view;
     }
 }
