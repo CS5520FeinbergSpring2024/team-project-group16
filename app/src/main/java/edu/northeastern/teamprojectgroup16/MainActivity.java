af4d0d6de9cd049e890aea693c37bf22f05750df
cecilia-uu <huikong1996@163.com>
2024-04-06 12:01:58 -0700

    make home page show properly

--- app/src/main/java/edu/northeastern/teamprojectgroup16/MainActivity.java
+++ app/src/main/java/edu/northeastern/teamprojectgroup16/MainActivity.java
@@ -1,6 +1,12 @@
 package edu.northeastern.teamprojectgroup16;
 
-import static androidx.core.content.ContentProviderCompat.requireContext;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -9,26 +15,13 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import edu.northeastern.teamprojectgroup16.databinding.ActivityCheckIndividualPostBinding;
 import edu.northeastern.teamprojectgroup16.databinding.ActivityMainBinding;
 import edu.northeastern.teamprojectgroup16.model.ServerModel;
 
